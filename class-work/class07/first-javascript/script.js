ad061eb01525c33ab9e93498e5163090c6ccaf0a
davidqxp <nycdavidqiu@hotmail.com>
2016-04-04 18:14:53 -0400

    commit!

---
+++ class-work/class07/first-javascript/script.js
@@ -0,0 +1,20 @@
+ //alert('this is an alert. this is annoying');
+
+ //document.write('writing to the document, like a boss');
+
+document.write(10 * 10);
+document.write(100 / 10);
+document.write(50 - 17);
+document.write(50 + 17);
+document.write('David Qiu');
+
+var num1 = 10 * 10;
+document.write(num1);
+
+var qiu = 'david'
+document.write(qiu)
+var 	david = 'qiu';
+document.write(david)
+
+document.getElementById('title').style.color='red';
+document.getElementById('text-chunk').style.background='red';
